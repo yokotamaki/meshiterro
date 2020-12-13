3df56c382c10aed086f953a0246bfadb95a2cbe2
yokotamaki <ymaki0626@gmail.com>
2020-12-13 12:40:05 +0000

    4章 コメントいいね機能を作ろう（コントローラの作成、ルーティングの設定）

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
 
   root 'post_images#index'
   devise_for :users
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
-
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resources :post_comments, only: [:create, :destroy]
+  end
 end
 
 
